0a68e5775bf0e0df1780f8c339a945e6d324186c
Sharu <sharanyar1994@gmail.com>
2024-09-24 17:38:33 +0530

    Small Chnages

--- taskmate/urls.py
+++ taskmate/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from todolist_app import views as todolist_views
 from django.conf import settings # new
-from  django.conf.urls.static import static #new
+from django.conf.urls.static import static #new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
